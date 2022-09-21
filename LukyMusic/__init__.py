f9ff2be955e94166a6228d6940967d647d97e911
lukymura <113297354+Lukymura@users.noreply.github.com>
2022-09-22 02:28:57 +0700

    Create __init__.py

---
+++ LukyMusic/__init__.py
@@ -0,0 +1,41 @@
+from LukyMusic.core.bot import LukyMusicBot
+from LukyMusic.core.dir import dirr
+from LukyMusic.core.git import git
+from LukyMusic.core.userbot import Userbot
+from LukyMusic.misc import dbb, heroku, sudo
+from aiohttp import ClientSession
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = LukyMusicBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+
+aiohttpsession = ClientSession()
